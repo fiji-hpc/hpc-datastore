66bca07ff81d3ebdd8af5efdcb00d78ac1409727
Jan Kožusznik <jan@kozusznik.cz>
2021-07-14 14:06:51 +0200

    feat: Do not create new dataset with blocks
    Resolves: #1624

--- src/main/java/cz/it4i/fiji/datastore/CreateNewDatasetTS.java
+++ src/main/java/cz/it4i/fiji/datastore/CreateNewDatasetTS.java
@@ -7,52 +7,41 @@
  ******************************************************************************/
 package cz.it4i.fiji.datastore;
 
+import static bdv.img.n5.BdvN5Format.DATA_TYPE_KEY;
+import static bdv.img.n5.BdvN5Format.DOWNSAMPLING_FACTORS_KEY;
+import static bdv.img.n5.BdvN5Format.getPathName;
 import static cz.it4i.fiji.datastore.DatasetPathRoutines.getXMLPath;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import net.imglib2.Dimensions;
 import net.imglib2.FinalDimensions;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.img.Img;
-import net.imglib2.img.list.AbstractListImg;
-import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
 
 import org.janelia.saalfeldlab.n5.Compression;
 import org.janelia.saalfeldlab.n5.DataType;
 import org.janelia.saalfeldlab.n5.N5FSWriter;
 import org.janelia.saalfeldlab.n5.N5Writer;
-import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 
 import bdv.export.ExportMipmapInfo;
-import bdv.export.ExportScalePyramid.AfterEachPlane;
-import bdv.export.ExportScalePyramid.LoopbackHeuristic;
-import bdv.export.ProgressWriter;
-import bdv.export.n5.WriteSequenceToN5;
-import bdv.img.n5.BdvN5Format;
 import bdv.img.n5.N5ImageLoader;
 import lombok.Builder;
 import lombok.NonNull;
 import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.XmlIoSpimData;
-import mpicbg.spim.data.generic.sequence.ImgLoaderHint;
+import mpicbg.spim.data.generic.sequence.BasicViewSetup;
 import mpicbg.spim.data.registration.ViewRegistration;
 import mpicbg.spim.data.registration.ViewRegistrations;
 import mpicbg.spim.data.sequence.Angle;
 import mpicbg.spim.data.sequence.Channel;
 import mpicbg.spim.data.sequence.Illumination;
-import mpicbg.spim.data.sequence.ImgLoader;
 import mpicbg.spim.data.sequence.SequenceDescription;
-import mpicbg.spim.data.sequence.SetupImgLoader;
 import mpicbg.spim.data.sequence.TimePoint;
 import mpicbg.spim.data.sequence.TimePoints;
 import mpicbg.spim.data.sequence.ViewSetup;
@@ -60,6 +49,9 @@ import mpicbg.spim.data.sequence.VoxelDimensions;
 
 
 public class CreateNewDatasetTS {
+
+	private static final String MULTI_SCALE_KEY = "multiScale";
+	private static final String RESOLUTION_KEY = "resolution";
 
 	public void run(Path path, N5Description dsc) throws IOException,
 		SpimDataException
@@ -85,31 +77,70 @@ public class CreateNewDatasetTS {
 		Collection<ViewSetup> viewSetups = generateViewSetups(dimensions,
 			voxelDimensions, channels, angles);
 
-		Map<Integer, ExportMipmapInfo> exportMipmapInfo =
-			assignMipmapInfoToViewSetups(viewSetups, mipmapInfo);
 		SequenceDescription sequenceDescription = new SequenceDescription(
-			new TimePoints(timepointsCol), viewSetups, new ImgLoaderImpl<>(N5Utils
-				.type(voxelType), dimensions));
-		WriteSequenceToN5.writeN5File(sequenceDescription, exportMipmapInfo,
-			compression, pathToDir.toFile(), new LoopbackHeuristicAdapter(),
-			new AfterEachPlaneAdapter(), 1, new ProgressWriterAdapter());
+			new TimePoints(timepointsCol), viewSetups, null);
+
+		final List<Integer> setupIds = sequenceDescription.getViewSetupsOrdered()
+			.stream().map(BasicViewSetup::getId).collect(Collectors.toList());
+		N5Writer n5 = new N5FSWriter(pathToDir.toFile().getAbsolutePath());
+		final int[][] resolutions = mipmapInfo.getExportResolutions();
+		final int[][] subdivisions = mipmapInfo.getSubdivisions();
+		int n = 3;
+		// write Mipmap descriptions
+		for (final int setupId : setupIds) {
+			final String pathName = getPathName(setupId);
+			final int[][] downsamplingFactors = mipmapInfo.getExportResolutions();
+			n5.createGroup(pathName);
+			n5.setAttribute(pathName, DOWNSAMPLING_FACTORS_KEY, downsamplingFactors);
+			n5.setAttribute(pathName, DATA_TYPE_KEY, voxelType);
+		}
+		final List<Integer> timepointIds = sequenceDescription.getTimePoints()
+			.getTimePointsOrdered().stream().map(TimePoint::getId).collect(Collectors
+				.toList());
+		for (final int timepointId : timepointIds) {
+
+			// assemble the viewsetups that are present in this timepoint
+			for (final int setupId : setupIds) {
+				final int numLevels = mipmapInfo.getNumLevels();
+				for (int level = 0; level < numLevels; ++level) {
+					long[] dims = new long[n];
+					final int[] factor = resolutions[level];
+					final long size = Intervals.numElements(factor);
+					final boolean fullResolution = size == 1;
+					System.arraycopy(dimensions, 0, dims, 0, n);
+					if (!fullResolution) {
+						for (int d = 0; d < n; ++d) {
+							dims[d] = Math.max(dims[d] / factor[d], 1);
+						}
+					}
+					n5.createDataset(getPathName(setupId, timepointId, level), dims,
+						subdivisions[level], voxelType, compression);
+
+				}
+
+				// additional attributes for paintera compatibility
+				final String pathName = getPathName(setupId, timepointId);
+				n5.createGroup(pathName);
+				n5.setAttribute(pathName, MULTI_SCALE_KEY, true);
+				final VoxelDimensions voxelSize = sequenceDescription.getViewSetups()
+					.get(setupId).getVoxelSize();
+				if (voxelSize != null) {
+					final double[] resolution = new double[voxelSize.numDimensions()];
+					voxelSize.dimensions(resolution);
+					n5.setAttribute(pathName, RESOLUTION_KEY, resolution);
+				}
+				for (int l = 0; l < resolutions.length; ++l)
+					n5.setAttribute(getPathName(setupId, timepointId, l),
+						DOWNSAMPLING_FACTORS_KEY, resolutions[l]);
+			}
+		}
+
+
 		sequenceDescription = new SequenceDescription(new TimePoints(timepointsCol),
 			viewSetups, new N5ImageLoader(pathToDir.toFile(), sequenceDescription));
 		SpimData result = new SpimData(pathToDir.toFile(), sequenceDescription,
 			new ViewRegistrations(generateViewRegistrations(timepointsCol,
 				viewSetups)));
-		removeAllBlocks(pathToDir, viewSetups, timepointsCol);
-		return result;
-	}
-
-	private Map<Integer, ExportMipmapInfo> assignMipmapInfoToViewSetups(
-		Collection<ViewSetup> viewSetups, ExportMipmapInfo mipmapInfo)
-	{
-
-		Map<Integer, ExportMipmapInfo> result = new HashMap<>();
-		for (ViewSetup viewSetup : viewSetups) {
-			result.put(viewSetup.getId(), mipmapInfo);
-		}
 		return result;
 	}
 
@@ -145,25 +176,6 @@ public class CreateNewDatasetTS {
 		return result;
 	}
 
-	private void removeAllBlocks(Path pathToDir,
-		Collection<ViewSetup> viewSetups, final Collection<TimePoint> timepointsCol)
-		throws IOException
-	{
-		N5Writer writer = new N5FSWriter(pathToDir.toString());
-		for (TimePoint timePoint : timepointsCol) {
-			for (ViewSetup viewSetup : viewSetups) {
-				String path = BdvN5Format.getPathName(viewSetup.getId(), timePoint
-					.getId());
-				for (String levelPath : writer.list(path)) {
-					String subPath = String.format("%s/%s", path, levelPath);
-					for (String datasetPath : writer.list(subPath)) {
-						writer.remove(String.format("%s/%s", subPath, datasetPath));
-					}
-				}
-			}
-		}
-	}
-
 	@Builder
 	public static class N5Description {
 
@@ -193,121 +205,5 @@ public class CreateNewDatasetTS {
 
 	}
 
-	private static class LoopbackHeuristicAdapter implements LoopbackHeuristic {
-
-		@Override
-		public boolean decide(RandomAccessibleInterval<?> originalImg,
-			int[] factorsToOriginalImg, int previousLevel,
-			int[] factorsToPreviousLevel, int[] chunkSize)
-		{
-			return false;
-		}
-	}
-
-	private static class AfterEachPlaneAdapter implements AfterEachPlane {
-
-		@Override
-		public void afterEachPlane(boolean usedLoopBack) {
-			// it is only adapter
-		}
-
-	}
-
-	private static class ProgressWriterAdapter implements ProgressWriter {
-
-		@Override
-		public PrintStream out() {
-			return System.out;
-		}
-
-		@Override
-		public PrintStream err() {
-			return System.err;
-		}
-
-		@Override
-		public void setProgress(double completionRatio) {
-			// it is only adapter
-		}
-	}
-
-	private static class ImgLoaderImpl<T> implements ImgLoader {
-
-		private SetupImgLoader<T> loader;
-
-		public ImgLoaderImpl(T type, long[] dimensions) {
-			loader = new SetupImgLoaderImpl<>(type, dimensions);
-		}
-
-		@Override
-		public SetupImgLoader<?> getSetupImgLoader(int setupId) {
-			return loader;
-		}
-
-		private static class SetupImgLoaderImpl<U> implements SetupImgLoader<U> {
-
-			private U type;
-			private RandomAccessibleInterval<U> interval;
-
-			public SetupImgLoaderImpl(U type, long[] dimensions) {
-				this.type = type;
-				this.interval = new EmptyImg<>(type, dimensions);
-			}
-
-			@Override
-			public RandomAccessibleInterval<U> getImage(int timepointId,
-				ImgLoaderHint... hints)
-			{
-				return interval;
-			}
-
-			@Override
-			public U getImageType() {
-				return type;
-			}
-
-			@Override
-			public RandomAccessibleInterval<FloatType> getFloatImage(int timepointId,
-				boolean normalize, ImgLoaderHint... hints)
-			{
-				return null;
-			}
-
-			@Override
-			public Dimensions getImageSize(int timepointId) {
-				return null;
-			}
-
-			@Override
-			public VoxelDimensions getVoxelSize(int timepointId) {
-				return null;
-			}
-		}
-	}
-
-	private static class EmptyImg<T> extends AbstractListImg<T> {
-
-		private T type;
-
-		public EmptyImg(T type, long[] size) {
-			super(size);
-			this.type = type;
-		}
-
-		@Override
-		public Img<T> copy() {
-			return this;
-		}
-
-		@Override
-		protected T get(int index) {
-			return type;
-		}
-
-		@Override
-		protected void set(int index, T value) {
-
-		}
-	}
 
 }
