f4bb2a7600c2afda4de6fcb2145959068945ff17
Jan Kožusznik <jan@kozusznik.cz>
2021-12-14 14:58:56 +0100

    Added html output.
    Resolves: #1663

--- src/main/java/cz/it4i/fiji/datastore/security/SecurityEndpoint.java
+++ src/main/java/cz/it4i/fiji/datastore/security/SecurityEndpoint.java
@@ -12,6 +12,8 @@ import static java.lang.String.format;
 import io.vertx.core.json.JsonObject;
 
 import java.net.URI;
+import java.util.Collection;
+import java.util.HashSet;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -19,8 +21,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
@@ -36,19 +40,20 @@ public class SecurityEndpoint {
 
 	@GET()
 	@Path("{" + OAUTH_SERVER + "}")
-	public Response get(@Context UriInfo request,
+	public Response get(@Context UriInfo requestURI, @Context HttpHeaders headers,
 		@PathParam(OAUTH_SERVER) String oauthServer, @QueryParam(CODE) String code)
 	{
-
 		//@formatter:off
 		return registry.findServer(oauthServer)
-				    .map(s -> processRequest(request, s, code))
+				    .map(s -> processRequest(new HashSet<>( headers.getAcceptableMediaTypes()),requestURI, s, code))
 				    .orElseGet(() -> oauthServerNotExists(oauthServer));
 		//@formatter:on
 
 	}
 
-	private Response processRequest(UriInfo request, OAuthServer s, String code) {
+	private Response processRequest(Collection<MediaType> acceptedTypes,
+		UriInfo request, OAuthServer s, String code)
+	{
 		if (code == null || code.isBlank()) {
 			return redirectToOAuthServerLogin(request, s);
 		}
@@ -63,13 +68,71 @@ public class SecurityEndpoint {
 				"OAuth server request error").build();
 		}
 
+		ResponseBuilder responseBuilder = Response.ok();
+		if (acceptedTypes.contains(MediaType.APPLICATION_JSON_TYPE)) {
+			getResponseAsJSON(responseBuilder, userInfo, tokens);
+		}
+		else if (acceptedTypes.contains(MediaType.TEXT_HTML_TYPE)) {
+			getResponseAsHTML(responseBuilder, userInfo, tokens);
+		}
+		
+		return responseBuilder.build();
+	}
+
+	private void getResponseAsHTML(ResponseBuilder responseBuilder,
+		UserInfo userInfo, UserTokens tokens)
+	{
+		StringBuilder sb = new StringBuilder();
+		// @formatter:off
+		sb.append(
+			"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en-gb\" lang=\"en-gb\" dir=\"ltr\">").append('\n')
+			.append("<head>").append('\n')
+			.append("<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />").append('\n')
+			.append("</head>").append('\n')
+			.append("<body>").append('\n');
+			
+			copyToClipboardJS(sb);
+			sb
+			.append("<h1>Authentication info</h1>").append('\n')
+			.append("<div>User ID: <input  type=\"text\" value=\"").append(userInfo.getUserId())
+			.append("\" id=\"userID\" readonly>")
+			.append("<button onclick=\"myFunction('userID')\">Copy text</button></div>").append('\n')
+			.append("<div>Access token: <input size=\"70\" type=\"text\" value=\"").append(tokens.getAccessToken())
+			.append("\" id=\"accessToken\" readonly>")
+			.append("<button onclick=\"myFunction('accessToken')\">Copy text</button></div>").append('\n')
+			.append("<div>Refresh token: <input size=\"70\" type=\"text\" value=\"").append(tokens.getRefreshToken())
+			.append("\" id=\"refreshToken\" readonly>")
+			.append("<button onclick=\"myFunction('refreshToken')\">Copy text</button></div>").append('\n')
+			.append("</body>").append('\n');
+		// @formatter:on
+		responseBuilder.type(MediaType.TEXT_HTML_TYPE).entity(sb.toString());
+	}
+
+	private void copyToClipboardJS(StringBuilder sb) {
+	// @formatter:off
+		sb
+		.append("<script>\n")
+		.append("function myFunction(myInput) {\n")
+		.append("  var copyText = document.getElementById(myInput);\n")
+		.append("  copyText.select();\n")
+		.append("  copyText.setSelectionRange(0, 99999); /* For mobile devices */\n")
+		.append("  /* Copy the text inside the text field */\n")
+		.append("  navigator.clipboard.writeText(copyText.value);\n")
+		.append("  /* Alert the copied text */\n")
+		.append("  alert(\"Copied the text: \" + copyText.value);\n")
+		.append("}")
+		.append("</script>\n");
+	
+	}
+
+	private void getResponseAsJSON(ResponseBuilder responseBuilder,
+		UserInfo userInfo, UserTokens tokens)
+	{
 		JsonObject jsonObj = new JsonObject();
 		jsonObj.put("userID", userInfo.getUserId());
 		jsonObj.put("accessToken", tokens.getAccessToken());
 		jsonObj.put("refreshToken", tokens.getRefreshToken());
-		
-		return Response.ok().type(MediaType.APPLICATION_JSON).entity(jsonObj
-			.toString()).build();
+		responseBuilder.type(MediaType.APPLICATION_JSON).entity(jsonObj.toString());
 	}
 
 	private static Response redirectToOAuthServerLogin(UriInfo request,
