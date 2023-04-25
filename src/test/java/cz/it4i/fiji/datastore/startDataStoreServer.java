4c5a0c950fc60e0114764508ef083f98b68782f9
Vladimír Ulman <ulman@fi.muni.cz>
2023-04-25 18:32:48 +0200

    ADD: a "test" file that start Quarkus application and
    waits until it is closed (or any text is given to it on the command line)

---
+++ src/test/java/cz/it4i/fiji/datastore/startDataStoreServer.java
@@ -0,0 +1,19 @@
+package cz.it4i.fiji.datastore;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@QuarkusTest()
+public class startDataStoreServer {
+
+	@Test
+	public void testRunning() throws IOException {
+		System.out.println("just close me from IDE when done...");
+		new BufferedReader(new InputStreamReader(System.in)).readLine();
+		//NB: makes it wait forever...
+	}
+}
