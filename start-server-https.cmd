bb1d1548a06b71d9bfcf24d87c08e69e0e900071
Vladimír Ulman <ulman@fi.muni.cz>
2024-02-22 16:20:24 +0100

    Cosmetics: tab vs spaces, args re-ordered, start-server help re-worded

--- start-server-https.cmd
+++ start-server-https.cmd
@@ -6,11 +6,14 @@ set clientID="554143119581-1kmphimgugsrfm45rd65bdja8h3bh27k.apps.googleuserconte
 set clientSecret="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"
 
 java ^
--Xmx1G -Dquarkus.datasource.jdbc.url="jdbc:h2:%myVar%/myDb;MODE=PostgreSQL;INIT=CREATE SCHEMA IF NOT EXISTS datastore" ^
--Ddatastore.path=%myVar% -Dquarkus.http.host=%ip% -Dquarkus.http.port=%port% -Ddatastore.ports=%pports% -Dquarkus.http.ssl-port=%port% ^
+-Xmx1G ^
+-Dquarkus.datasource.jdbc.url="jdbc:h2:%myVar%/myDb;MODE=PostgreSQL;INIT=CREATE SCHEMA IF NOT EXISTS datastore" ^
+-Ddatastore.path=%myVar% -Ddatastore.ports=%pports% ^
+-Dquarkus.http.host=%ip% -Dquarkus.http.port=%port% ^
+-Dquarkus.http.ssl-port=%port% ^
 -Dquarkus.http.ssl.certificate.key-store-file=META-INF/resources/server.keystore ^
--Dcz.it4i.fiji.datastore.security.servers=google,sub,https://accounts.google.com/o/oauth2/v2/auth,https://www.googleapis.com/oauth2/v3/userinfo,https://www.googleapis.com/oauth2/v4/token,%clientID%,%clientSecret% ^
 -Dquarkus.http.limits.max-body-size=200M ^
+-Dcz.it4i.fiji.datastore.security.servers=google,sub,https://accounts.google.com/o/oauth2/v2/auth,https://www.googleapis.com/oauth2/v3/userinfo,https://www.googleapis.com/oauth2/v4/token,%clientID%,%clientSecret% ^
 -Dcz.it4i.fiji.datastore.security.users=google:100198484073624931665:1:write ^
 -Dcz.it4i.fiji.datastore.security.on="YES" ^
 -Xverify:none ^
